8fdf43476eaf8c32602a8f4fba4381155a8eec34
KARTIK BHATT <94695384+katzByte007@users.noreply.github.com>
2023-11-15 23:01:07 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 # main.py
 
+# main.py
+
 from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
 from llama_index.vector_stores import ChromaVectorStore
 from llama_index.storage.storage_context import StorageContext
@@ -8,31 +10,29 @@ from llama_index.llms import OpenAI
 from IPython.display import Markdown, display
 import chromadb
 
-# Define embedding function
+# Create an embedding model
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
-documents = SimpleDirectoryReader(
-    input_files=["Orca_paper.pdf"]
-).load_data()
+# Read documents from a simple directory
+documents = SimpleDirectoryReader(input_files=["Orca_paper.pdf"]).load_data()
 
-# CHROMA-DB
+# Create a temporary ChromaDB client and collection
 chroma_client = chromadb.EphemeralClient()
 chroma_collection = chroma_client.create_collection("orca_paper")
 
-# Set up ChromaVectorStore and load in data
+# Set up a ChromaVectorStore and load data
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
 service_context = ServiceContext.from_defaults(embed_model=embed_model)
 
+# Create a VectorStoreIndex from the loaded documents
 index = VectorStoreIndex.from_documents(documents,
                                         storage_context=storage_context,
                                         service_context=service_context)
 
-# Query Data
+# Query the data
 query_engine = index.as_query_engine()
-
 response = query_engine.query("What are some of the main contributions of this new Orca model?")
 
+# Display the response using Markdown
 display(Markdown(f"{response}"))
